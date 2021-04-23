f541934980b138d0f370e2e13444f26bffcfbd2c
Seyram Kwame Attah <seyramkwameattah@gmail.com>
2021-04-23 22:55:19 +0000

    First commit

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+#Beginners guide to python and version control using git
+
+print("Welcome to Python Programming")
+print("The files are now tracked for version control")
+print("Hello to Python and Version Control")
+
+print('Closing Application')
+
---
+++ module_variables.py
@@ -0,0 +1,9 @@
+#Create variables in python
+
+print("Beginners guide to python programming\n")
+first_name ='Seyram Kwame'
+last_name ="Attah"
+course_offered = "Offensive Security Web Expert Course"
+
+print(first_name + ' ' + last_name + ' ' + "is pursuing" + ' ' + course_offered)
+print("End program")
